778a232106cfa06626c91da65ea5f0b793380491
moisesgianizelli <ork.mgm@gmail.com>
2024-04-26 14:29:12 +0100

    Accordian component done

---
+++ src/component/accordian/data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accordion components?",
+    answer:
+      "Accordion components are user interface elements used for organizing and presenting data",
+  },
+  {
+    id: "2",
+    question: "What are they used for?",
+    answer:
+      "They are used in various context such as FAQS, product description..",
+  },
+  {
+    id: "3",
+    question: "As a musical instrument?",
+    answer: "it is also a musical instrument",
+  },
+];
+export default data;
