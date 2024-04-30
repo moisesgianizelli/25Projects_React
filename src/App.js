f4e33fc7672479bf23e78161bad4a00459c44351
moisesgianizelli <ork.mgm@gmail.com>
2024-04-30 19:25:17 +0100

    random-color component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Accordian from "./component/accordian";
+import RandomColor from "./component/random-color";
 
 function App() {
   return (
     <div className="App">
+      {/*Accordian*/}
       <Accordian />
+
+      {/*Accordian*/}
+      <RandomColor />
     </div>
   );
 }
